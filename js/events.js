79f1c3a6c24e4b2560abc6914cd8a999fe40581f
Dan Bardach <bardachd@xavier.edu>
2018-06-30 18:10:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,16 +5,16 @@ function frameIt() {
   $("img") .addClass("tasty")
   return;
 }
-+function pressIt() {
-+  $('form').on('keydown', (e) => {
+function pressIt() {
+  $('form').on('keydown', (e) => {
     if (e.which == 71) {
       alert('just a g thang')    }
  })
 }
 
 
-+function submitIt() {
-+  $('form').on('submit', () => {
-+    alert("Your form is going to be submitted now.")
-+  })
-+}
+function submitIt() {
+  $('form').on('submit', () => {
+    alert("Your form is going to be submitted now.")
+  })
+}
